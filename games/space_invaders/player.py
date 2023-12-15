f3c42770ba92b3c61b751784a45199ec1e048b13
max-meme <ziegenfredmax@gmail.com>
2023-12-15 18:10:25 +0100

    player movement

--- games/space_invaders/player.py
+++ games/space_invaders/player.py
@@ -1,4 +1,5 @@
 import pygame
+from bullet import Bullet
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, image, pos, constraints):
@@ -10,15 +11,41 @@ class Player(pygame.sprite.Sprite):
         self.constraints = constraints
         self.rect = self.image.get_rect()
         self.rect.midbottom = pos
-        self.speed = 5
+        self.speed_y = 0
+        self.speed_x = 0
+        self.bullets = []
 
     def move(self, keys, constraints):
         # TASK 1.1 - player movements
-        pass
+        if(keys[pygame.K_UP] or keys[pygame.K_w]):
+            self.speed_y = -5
+        elif(keys[pygame.K_DOWN] or keys[pygame.K_s]):
+            self.speed_y = 5
+        else: self.speed_y = 0
+
+        if(keys[pygame.K_RIGHT] or keys[pygame.K_d]):
+            self.speed_x = 5
+        elif(keys[pygame.K_LEFT] or keys[pygame.K_a]):
+            self.speed_x = -5
+        else: self.speed_x = 0
+
+        # clamp player coordinates to screen bounds
+        self.rect.x = min(self.rect.x, constraints[0] - self.rect.width)
+        self.rect.y = min(self.rect.y, constraints[1] - self.rect.height)
+        self.rect.x = max(self.rect.x, 0)
+        self.rect.y = max(self.rect.y, 0)
 
     def update(self, keys, bullets):
 
         # TASK 1.2 - fire bullets
+        if(keys[pygame.K_SPACE]):
+            self.bullets.append(Bullet(self.rect.midbottom, -10))
         # Move the player
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
         self.move(keys, self.constraints)
+
+        # Update Bulltes
+        for b in self.bullets:
+            b.update()
 
